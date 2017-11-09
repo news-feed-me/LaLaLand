fafc331a4ca34bb088696380f8872f54942aa1d3
asuzuki <asuzuki@sfu.ca>
2017-11-08 23:05:42 -0800

    fixed accidental overwrites

--- db/schema.rb
+++ db/schema.rb
@@ -27,12 +27,12 @@ ActiveRecord::Schema.define(version: 20171104010513) do
   add_index "subscribes", ["user_id", "subscription_id"], name: "index_subscribes_on_user_id_and_subscription_id", using: :btree
 
   create_table "subscriptions", primary_key: "subscription_id", force: :cascade do |t|
-    t.string   "name",           limit: 255
-    t.string   "url",            limit: 255
-    t.string   "geography",      limit: 255
-    t.string   "json_reference", limit: 255
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.string   "name",       limit: 255
+-    t.string   "url"
+-    t.string   "source_id"
+-    t.string   "category",   limit: 255
+-    t.datetime "created_at"
+-    t.datetime "updated_at"
   end
 
   create_table "users", primary_key: "user_id", force: :cascade do |t|
