34c62f54d0cdd2522989dec83d9936c770a03235
Dan Tang <dantang96@gmail.com>
2017-11-19 21:12:23 -0800

    UI changes

---
+++ test/models/user_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  #test "user should not save" do
+  #  assert true
+  #end
+end
