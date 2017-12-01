818d58d9f574f33d3bd2b2ba26d66483e0ece318
Cora Xiaotong Xu <asdfxu@qq.com>
2017-11-30 20:06:37 -0800

    unit testing working

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,10 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-
+  test "invalid without a password" do
+    u = users(:one)
+    assert !u.valid?, "user not valid it dosent have a password"
+  end
   #test "user should not save" do
   #  assert true
   #end
