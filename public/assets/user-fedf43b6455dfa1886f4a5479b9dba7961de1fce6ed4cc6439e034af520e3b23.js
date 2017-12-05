6e6fa9f6b79cf47161d6afd2a74e1b999492dd18
Asim <ahimani@sfu.ca>
2017-12-04 18:42:08 -0800

    Deploying to heroku

---
+++ public/assets/user-fedf43b6455dfa1886f4a5479b9dba7961de1fce6ed4cc6439e034af520e3b23.js
@@ -0,0 +1,35 @@
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk');
+
+function fbs_click() {u=location.href;t=document.title;window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer','toolbar=0,status=0,width=626,height=436');return false;}
+
+function changeColor(){
+  var body = document.getElementsByTagName("body");
+
+  return;
+}
+
+/*
+function favoriteClick(id, article_status){
+
+  if (article_status == false){
+
+    article_status = true;
+
+  }
+  else {
+
+    article_status = false;
+
+  }
+
+  return;
+}
+*/
+;
